0cfe87b2ca613c8db9f2bf9fe748337118ca5393
Saif Ullah Sajid <saifbinsajid7@gmail.com>
2020-05-14 16:42:49 +0500

    Added code

---
+++ StarWarsMovies/Views/FilmsTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilmsTableViewCell.swift
+//  StarWarsMovies
+//
+//  Created by Saif Ullah Sajid on 2019-09-04.
+//  Copyright © 2019 Coding Homies. All rights reserved.
+//
+
+import UIKit
+
+class FilmsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var filmTitle: UILabel!
+    @IBOutlet weak var filmDirector: UILabel!
+    @IBOutlet weak var releaseDate: UILabel!
+    
+}
